250920a2aa873e677a884338497e16046fbf46b3
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2020-04-16 16:44:50 +0300

    add a different index.ejs

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const connectionString = process.env.DATABASE_URL
+const { Pool } = require('pg')
+const pool = new Pool({connectionString: connectionString})
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -8,3 +11,5 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  
+  
