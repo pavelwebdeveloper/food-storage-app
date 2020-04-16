f3eaa4ba91e030e3ae92468d101dc60bdd116845
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2020-04-16 19:47:44 +0300

    fix load error

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+var app = express();
 const connectionString = process.env.DATABASE_URL
 const { Pool } = require('pg')
 const pool = new Pool({connectionString: connectionString})
 
-express()
+app
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
