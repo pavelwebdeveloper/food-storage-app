04361eafe7ea7b35cf0db2579494d4d4a6ff53ee
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2020-04-25 20:38:17 +0300

    changing-menu-and-adding-manage-items-page

--- index.js
+++ index.js
@@ -12,8 +12,14 @@ app
   .set('view engine', 'ejs')
   //.get('/', (req, res) => res.render('pages/index'))
   .get('/', getFoodStorageItems)
+  .get('/getmanagefoodstoragepage', getManageFoodStoragePage)
+  .get('/gethomepage', getHomePage)
   
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  
+  
+  
+  // The functions
   
   function getFoodStorageItems (req, res) {
 	  
@@ -46,6 +52,88 @@ app
     });
 	*/
   }
+  
+  
+  function getManageFoodStoragePage(req, res){
+	console.log("Getting items from DB");
+	
+	// This runs the query to get the hotdogs
+	
+  pool.query('SELECT * FROM items', function(err, result) {
+      if (err) {
+        return console.error('error running query', err);
+      }
+	  
+	  // Log this to the console for debugging purposes.
+    console.log("Back from DB with result:");
+	console.log(result.rows);
+	const items = result.rows;
+	console.log("items variable:");
+	console.log(items);
+	var infomessage = "";
+	
+	res.render('pages/manage_food_storage_page', {
+        items: items,
+		infomessage: infomessage
+    });
+	
+	//callback(null, result.rows);
+    });
+	
+/* This code is merely for testing purposes to test the app locally*/
+	  /*
+	  const items = [{id:1,itemname:'fish',amount:150},{id:2,itemname:'cheese',amount:30}]
+	console.log("items variable:");
+	console.log(items);
+	var infomessage = "";
+	
+	res.render('pages/manage_food_storage_page', {
+        items: items,
+		infomessage: infomessage
+    });
+*/
+}
+
+
+function getHomePage(req, res){
+	console.log("Getting items from DB");
+	
+	// This runs the query to get the hotdogs
+	
+  pool.query('SELECT * FROM items', function(err, result) {
+      if (err) {
+        return console.error('error running query', err);
+      }
+	  
+	  // Log this to the console for debugging purposes.
+    console.log("Back from DB with result:");
+	console.log(result.rows);
+	const items = result.rows;
+	console.log("items variable:");
+	console.log(items);
+	var infomessage = "";
+	
+	res.render('pages/home_page', {
+        items: items,
+		infomessage: infomessage
+    });
+	
+	//callback(null, result.rows);
+    });
+	
+/* This code is merely for testing purposes to test the app locally*/
+	 /* 
+	  const items = [{id:1,itemname:'fish',amount:150},{id:2,itemname:'cheese',amount:30}]
+	console.log("items variable:");
+	console.log(items);
+	var infomessage = "";
+	
+	res.render('pages/home_page', {
+        items: items,
+		infomessage: infomessage
+    });
+*/
+}
   
   
   
