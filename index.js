2db66e3bdb90397421c073b360185bfa16ceaea7
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2020-04-30 20:25:43 +0300

    added an add item function [adding-an-add-item-function]

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var app = express();
 const connectionString = process.env.DATABASE_URL
 const { Pool } = require('pg')
 const pool = new Pool({connectionString: connectionString})
+//var items = [{id:1,itemname:'fish',amount:150},{id:2,itemname:'cheese',amount:30}];
 
 app
   .use(express.static(path.join(__dirname, 'public')))
@@ -14,6 +15,7 @@ app
   .get('/', getFoodStorageItems)
   .get('/getmanagefoodstoragepage', getManageFoodStoragePage)
   .get('/gethomepage', getHomePage)
+  .post('/additem', addItem)
   
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
   
@@ -41,9 +43,10 @@ app
 	  });
 	  
 	  
+	  
 	  /* This code is merely for testing purposes to test the app locally*/
 	  /*
-	  const items = [{id:1,itemname:'fish',amount:150},{id:2,itemname:'cheese',amount:30}]
+	  //const items = [{id:1,itemname:'fish',amount:150},{id:2,itemname:'cheese',amount:30}]
 	console.log("items variable:");
 	console.log(items);
 	
@@ -75,14 +78,13 @@ app
 	res.render('pages/manage_food_storage_page', {
         items: items,
 		infomessage: infomessage
+		});
     });
 	
-	//callback(null, result.rows);
-    });
 	
 /* This code is merely for testing purposes to test the app locally*/
-	  /*
-	  const items = [{id:1,itemname:'fish',amount:150},{id:2,itemname:'cheese',amount:30}]
+	 /* 
+	 // const items = [{id:1,itemname:'fish',amount:150},{id:2,itemname:'cheese',amount:30}]
 	console.log("items variable:");
 	console.log(items);
 	var infomessage = "";
@@ -116,14 +118,14 @@ function getHomePage(req, res){
 	res.render('pages/home_page', {
         items: items,
 		infomessage: infomessage
+		});
     });
 	
-	//callback(null, result.rows);
-    });
 	
 /* This code is merely for testing purposes to test the app locally*/
-	 /* 
-	  const items = [{id:1,itemname:'fish',amount:150},{id:2,itemname:'cheese',amount:30}]
+	  
+	  //const items = [{id:1,itemname:'fish',amount:150},{id:2,itemname:'cheese',amount:30}]
+	  /*
 	console.log("items variable:");
 	console.log(items);
 	var infomessage = "";
@@ -133,6 +135,80 @@ function getHomePage(req, res){
 		infomessage: infomessage
     });
 */
+}
+
+
+function addItem(req, res) {
+		var infomessage = "";
+	console.log("Add Item Info:");
+	console.log(req.query.itemname);
+	console.log(req.query.itemamount);
+	if(!req.query.itemname || !req.query.itemamount) {
+	infomessage = "Please, provide all the required information.";
+		res.render('pages/manage_food_storage_page', {
+        infomessage: infomessage
+    });	
+	} else {
+		//var id = Math.floor(Math.random() * 10);
+		
+		// This runs the query to add an item
+  pool.query('INSERT INTO items (itemname, itemamount) VALUES ($1, $2)', [req.query.itemname, req.query.itemamount], function(err, result) {
+      if (err) {
+        return console.error('error running query', err);
+      }
+	  
+	  // Log this to the console for debugging purposes.
+    // Log this to the console for debugging purposes.
+    console.log("Back from DB with result of adding an item:");
+	console.log(result);
+	console.log(result.rowCount);
+	
+	// This runs the query to get the items
+  pool.query('SELECT * FROM items', function(err, result) {
+      if (err) {
+        return console.error('error running query', err);
+      }
+	  
+	  // Log this to the console for debugging purposes.
+    console.log("Back from DB with result !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!:");
+	console.log(result.rows);
+	const items = result.rows;
+	console.log("items variable: $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+	console.log(items);
+	infomessage = "You have successfully added " + req.query.itemname + " !";
+	
+	res.render('pages/manage_food_storage_page', {
+        items: items,
+		infomessage: infomessage
+    });
+
+    }); 
+    });
+	}
+	/* This code is merely for testing purposes to test the app locally*/
+	/*
+	{
+		
+		// This runs the query to add an item
+		var newItem = {id:0,itemname:'',amount:0};
+		console.log("items before adding a new item");
+		console.log(items);
+		newItem.id = items.length + 1;
+		newItem.itemname = req.query.itemname;
+		newItem.amount = req.query.itemamount;
+		items.push(newItem);
+		console.log("items after adding a new item");
+		console.log(items);
+		
+		
+		var infomessage = "";
+	
+	res.render('pages/manage_food_storage_page', {
+        items: items,
+		infomessage: infomessage
+    });		
+	}
+	*/
 }
   
   
