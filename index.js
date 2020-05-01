d903449762a944d1ce59db3a5d2169b999c2982e
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2020-05-01 12:47:58 +0300

    debugging

--- index.js
+++ index.js
@@ -265,7 +265,7 @@ function deleteItem(req, res) {
 	//callback(null, result.rows);
     });
 	
-	pool.query('ALTER SEQUENCE items RESTART;', function(err, result) {
+	pool.query('ALTER SEQUENCE items RESTART WITH 1;', function(err, result) {
       if (err) {
         return console.error('error running query', err);
       }
