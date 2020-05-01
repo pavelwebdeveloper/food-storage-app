fd2c750824e52060ddb8bd4c75506862cbef92c5
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2020-05-01 14:23:13 +0300

    debugging

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app
 	  // Log this to the console for debugging purposes.
     console.log("Back from DB with the number of items in the items table");
 	console.log(result.rows);
-	itemsnumber = result.rows;
+	itemsnumber = result.rows.count;
 	console.log("items number");
 	console.log(itemsnumber);
     });
