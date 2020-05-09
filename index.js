2b0746fb6d8bb954ae85111d3e40faa605819801
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2020-05-09 19:49:13 +0300

    trying to convert to number

--- index.js
+++ index.js
@@ -400,7 +400,7 @@ function deleteItem(req, res) {
 	console.log("oldamount variable:))))))))))))))))))))");
 	console.log(isNaN(oldamount));
 	
-	newamount = Number(oldamount + updateamount);
+	newamount = parseFloat(oldamount + updateamount);
 	console.log("newamount variable:))))))))))))))))))))");
 	console.log(isNaN(newamount));
 	
