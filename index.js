5540eca771893519199a3dd4c5bccb3bab14547f
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2020-05-16 17:09:26 +0300

    trying to fix the name in the success message

--- index.js
+++ index.js
@@ -447,7 +447,7 @@ function deleteItem(req, res) {
 	const items = result.rows;
 	console.log("items variable:");
 	console.log(items);
-	successmessage = "Success! You have successfully updated the amount of " + items.itemname;
+	successmessage = "Success! You have successfully updated the amount of " + items[req.query.id].itemname;
 	const itemname = "";
 	const itemamount = 0;
 	
